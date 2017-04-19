0060109756e2ae25ee6fd3d2f10b5e4da22217be
Emma Malysz <emalysz@usc.edu>
2017-04-18 18:21:25 -0700

    Add files via upload

---
+++ recurrent_CSCI201L_Project/src/recurrent_CSCI201L_Project/FacebookLoginServlet.java
@@ -0,0 +1,46 @@
+package recurrent_CSCI201L_Project;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SignUpServlet
+ */
+@WebServlet("/FacebookLoginServlet")
+public class FacebookLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("did we get here");
+		String errorMessage = "";
+
+		JDBCTest jdb = new JDBCTest();
+		String username = request.getParameter("username");
+		String type = request.getParameter("type");
+		String password = request.getParameter("password");
+		String email = "n/a";
+
+		if (type.equals("lender")) {
+			// Lender lender = new Lender(username, password, email);
+			jdb.addLender(username, password, email);
+			request.getSession().setAttribute("loggedUser", username);
+		} else {
+			// Renter renter = new Renter(username, password, email);
+			jdb.addRenter(username, password, email);
+			request.getSession().setAttribute("loggedUser", username);
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
